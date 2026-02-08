aeb20820ffcbb36e94403bfdfe83426f1584f892
Daniel de Morais <danielmoraissoares317@gmail.com>
2026-02-08 14:16:21 -0300

    Add files via upload

---
+++ test.ino
@@ -0,0 +1,17 @@
+#include "main.cpp"
+#include "BlinkLed.cpp"
+#include "BlinkLed.h"
+// C++ code
+//
+void setup()
+{
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000);
+}
